ec1031e566456140996df57d808bac1c6320372c
Atul-Chandran <atulchandran05@gmail.com>
2021-07-08 19:19:26 +0530

    Port changes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const http = require('http');
 const express = require('express')
 const app = express();
 
+require('dotenv').config()
+
+
 const controllers = require("./controller/gettingData.controller").data
 
 app.use(express.json())
@@ -9,9 +12,8 @@ const server = http.createServer(app);
 
 app.post('/', controllers.sendStockData)
 
-module.exports = server
-
-
-server.listen(3002, 'localhost', () => {
+server.listen(process.env.port, 'localhost', () => {
   console.log(`Server running at http://localhost:3002/`);
 });
+
+module.exports = server
